40cd36a6601d49d325904a5fd0e077ef6ff52fcf
kelvin34501 <kelvin34501@gmail.com>
2022-07-12 18:08:55 +0800

    lsp ignore

--- lang/lsp.el
+++ lang/lsp.el
@@ -1,3 +1,45 @@
 ;;; -*- lexical-binding: t; -*-
 
 (setq lsp-log-io t)
+
+;; dir local lsp ignored
+(put 'local/lsp-file-watch-ignored-directories 'safe-local-variable #'proper-list-p)
+(put 'local/lsp-file-watch-ignored-files 'safe-local-variable #'proper-list-p)
+(defun local/lsp-file-watch-ignored-directories-append-h ()
+  (if (boundp 'local/lsp-file-watch-ignored-directories)
+      (progn
+        (message "[hook]: local/lsp-file-watch-ignored-directories-append")
+        (require 'lsp-mode)
+        (if (not (boundp 'local/lsp-file-watch-ignored-directories-backup))
+            (setq local/lsp-file-watch-ignored-directories-backup (cl-copy-list (lsp-file-watch-ignored-directories))))
+        (let ((local-list local/lsp-file-watch-ignored-directories))
+          (while local-list
+            (add-to-list 'lsp-file-watch-ignored-directories (car local-list))
+            (setq local-list (cdr local-list)))))))
+(defun local/lsp-file-watch-ignored-files-append-h ()
+  (if (boundp 'local/lsp-file-watch-ignored-files)
+      (progn
+        (message "[hook]: local/lsp-file-watch-ignored-files-append")
+        (require 'lsp-mode)
+        (if (not (boundp 'local/lsp-file-watch-ignored-files-backup))
+            (setq local/lsp-file-watch-ignored-files-backup (cl-copy-list (lsp-file-watch-ignored-files))))
+        (let ((local-list local/lsp-file-watch-ignored-files))
+          (while local-list
+            (add-to-list 'lsp-file-watch-ignored-files (car local-list))
+            (setq local-list (cdr local-list)))))))
+(add-hook 'projectile-after-switch-project-hook #'local/lsp-file-watch-ignored-directories-append-h)
+(add-hook 'projectile-after-switch-project-hook #'local/lsp-file-watch-ignored-files-append-h)
+(defun local/lsp-file-watch-ignored-directories-cleanup-h ()
+  (if (boundp 'local/lsp-file-watch-ignored-directories-backup)
+      (progn
+        (message "[hook]: local/lsp-file-watch-ignored-directories-cleanup")
+        (setq lsp-file-watch-ignored-directories (cl-copy-list local/lsp-file-watch-ignored-directories-backup))
+        (makunbound 'local/lsp-file-watch-ignored-directories-backup))))
+(defun local/lsp-file-watch-ignored-files-cleanup-h ()
+  (if (boundp 'local/lsp-file-watch-ignored-files-backup)
+      (progn
+        (message "[hook]: local/lsp-file-watch-ignored-files-cleanup")
+        (setq lsp-file-watch-ignored-files (cl-copy-list local/lsp-file-watch-ignored-files-backup))
+        (makunbound 'local/lsp-file-watch-ignored-files-backup))))
+(add-hook 'projectile-before-switch-project-hook #'local/lsp-file-watch-ignored-directories-cleanup-h)
+(add-hook 'projectile-before-switch-project-hook #'local/lsp-file-watch-ignored-files-cleanup-h)
--- lang/python.el
+++ lang/python.el
@@ -4,15 +4,15 @@
 ;(setq lsp-pyright-diagnostic-mode "workspace")
 (setq lsp-pyright-auto-import-completions nil)
 
+(put 'local-black-line-length 'safe-local-variable #'integerp)
 (set-formatter! 'black ; overwrite prefefined black formatter
   '("black" "-q"
     ("--line-length" "%s" (if (boundp 'local-black-line-length) local-black-line-length 120))
     "-")
   )
-(put 'local-black-line-length 'safe-local-variable #'integerp)
 
 (put 'local-pyvenv-path 'safe-local-variable #'stringp)
-(defun local-pyvenv-auto-activate ()
+(defun local-pyvenv-auto-activate-h ()
   (if (boundp 'local-pyvenv-path)
       (progn
         (message "[hook]: local-pyvenv-path activate: %s" local-pyvenv-path)
@@ -22,7 +22,7 @@
         )
     )
   )
-(defun local-pyvenv-auto-deactivate ()
+(defun local-pyvenv-auto-deactivate-h ()
   (if (boundp 'local-pyvenv-path)
       (progn
         (message "[hook]: local-pyvenv-path deactivate")
@@ -31,5 +31,5 @@
     )
   )
 
-(add-hook 'projectile-after-switch-project-hook #'local-pyvenv-auto-activate)
-(add-hook 'projectile-before-switch-project-hook #'local-pyvenv-auto-deactivate)
+(add-hook 'projectile-after-switch-project-hook #'local-pyvenv-auto-activate-h)
+(add-hook 'projectile-before-switch-project-hook #'local-pyvenv-auto-deactivate-h)
